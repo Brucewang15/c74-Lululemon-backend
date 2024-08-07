c4ebb9a3e92165ae06e540bef5956d83b8181745
jayzchen2001 <jayzchen2001@gmail.com>
2024-08-07 18:02:54 -0400

    added inv

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 import user from './user'
 import auth from './auth'
+import inventory from './inventory'
 
 const rootRouter = Router()
 
 rootRouter.use('/user', auth)
+rootRouter.use('/inv', inventory)
 
 export default rootRouter
