c81cc7b8f34c3c1c5f98595dc581494ec7a3a336
Xiaojin Cai <xcai0511@gmail.com>
2024-08-19 23:10:11 -0400

    updated openai prompt

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,19 +1,19 @@
-import { Router } from 'express'
-import user from './user'
-import auth from './auth'
-import change from './change'
-import inventory from './inventory'
-import cart from './cart'
-import imageAI from './imageAI'
-import openAI from './openAI'
+import { Router } from "express";
+import user from "./user";
+import auth from "./auth";
+import change from "./change";
+import inventory from "./inventory";
+import cart from "./cart";
+import imageAI from "./imageAI";
+import openAI from "./openAI";
 
-const rootRouter = Router()
-rootRouter.use('/auth', auth)
-rootRouter.use('/user', user)
-rootRouter.use('/change', change)
-rootRouter.use('/inv', inventory)
-rootRouter.use('/cart', cart)
-rootRouter.use('/image-ai', imageAI)
-rootRouter.use('/openAI', openAI)
+const rootRouter = Router();
+rootRouter.use("/auth", auth);
+rootRouter.use("/user", user);
+rootRouter.use("/change", change);
+rootRouter.use("/inv", inventory);
+rootRouter.use("/cart", cart);
+rootRouter.use("/image-ai", imageAI);
+rootRouter.use("/openAI", openAI);
 
-export default rootRouter
+export default rootRouter;
