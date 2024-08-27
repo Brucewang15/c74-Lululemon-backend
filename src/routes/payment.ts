9de192863ff8722dd2b698a516352017ee977e81
chenj605 <jianjia.chen@mail.utoronto.ca>
2024-08-27 18:29:18 -0400

    added support for stripe form

--- src/routes/payment.ts
+++ src/routes/payment.ts
@@ -4,4 +4,8 @@ import { PaymentController } from "../controllers/PaymentController";
 const router = Router();
 router.post("/", PaymentController.createPayment);
 
+router.post("/stripe", PaymentController.createStripePayment);
+
+router.post("/stripe/paid", PaymentController.completeStripePayment);
+
 export default router;
